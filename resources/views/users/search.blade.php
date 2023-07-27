404e28ace04e9bbbbc640d10918b465c8ed9a49e
Kredo-Garry <kredoteacher.grodriguez@gmail.com>
2023-07-27 10:35:49 +0800

    added a paragraph tag in line no 7

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <div class="row justify-content-center">
+        <p>This will display all the users being search</p>
         <div class="col-5">
             <p class="h5 text-muted mb-4">Search results for "<span class="fw-bold">{{ $search }}</span>"</p>
 
